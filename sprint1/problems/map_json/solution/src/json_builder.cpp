7baff57f361f03d714769a838cd35c0e3f7ac2fd
Stanislav Polischuk <polischuk.stanislav@gmail.com>
2024-11-25 02:08:02 +0400

    map_json impl

---
+++ sprint1/problems/map_json/solution/src/json_builder.cpp
@@ -0,0 +1,91 @@
+#include "json_builder.h"
+#include <map>
+#include <sstream>
+
+namespace jsonBuilder{
+    json::value BuildMapListJson(const model::Game& game){
+        json::array json_arr;
+
+        for(const auto& item : game.GetMaps()){
+            json::object map;
+            map["id"] = *item.GetId();
+            map["name"] = item.GetName();
+            json_arr.emplace_back(std::move(map));
+        }
+        return json_arr;
+    }
+
+    void AddRoadsToJson(const model::Map& map, json::value& root){
+        json::array roads;
+        for(const auto& item : map.GetRoads()){
+            json::object road;
+            road["x0"] = item.GetStart().x;
+            road["y0"] = item.GetStart().y;
+            if(item.IsVertical()){
+                road["y1"] = item.GetEnd().y;
+            }else {
+                road["x1"] = item.GetEnd().x;
+            }
+            roads.emplace_back(std::move(road));
+        }
+        root.as_object()["roads"] = std::move(roads);
+    }
+
+    void AddBuildingsToJson(const model::Map& map, json::value& root){
+        json::array buildings;
+        for(const auto& item : map.GetBuildings()){
+            json::object building;
+            building["x"] = item.GetBounds().position.x;
+            building["y"] = item.GetBounds().position.y;
+            building["w"] = item.GetBounds().size.width;
+            building["h"] = item.GetBounds().size.height;
+            buildings.emplace_back(std::move(building));
+        }
+        root.as_object()["buildings"] = std::move(buildings);
+    }
+
+    void AddOfficesToJson(const model::Map& map, json::value& root){
+        json::array offices;
+        for(const auto& item : map.GetOffices()){
+            json::object office;
+            office["id"] = (*item.GetId()).c_str();
+            office["x"] = item.GetPosition().x;
+            office["y"] = item.GetPosition().y;
+            office["offsetX"] = item.GetOffset().dx;
+            office["offsetY"] = item.GetOffset().dy;
+            offices.emplace_back(std::move(office));
+        }
+        root.as_object()["offices"] = std::move(offices);
+    }
+
+    json::value ConvertMapToJson(const model::Map& map){
+        json::value map_json;
+        map_json.as_object()["id"] = *map.GetId();
+        map_json.as_object()["name"] = map.GetName();
+        AddRoadsToJson(map, map_json);
+        AddBuildingsToJson(map, map_json);
+        AddOfficesToJson(map, map_json);
+        return map_json;
+    }
+
+    json::value BuildMapNotFoundJsonResponce(){
+        json::value root;
+        root.as_object()["code"] = "mapNotFound";
+        root.as_object()["message"] = "Map not found";
+        return root;
+    };
+
+    json::value BuildBadRequestJsonResponse(){
+        json::value root;
+        root.as_object()["code"] = "badRequest";
+        root.as_object()["message"] = "Bad request";
+        return root;
+    };
+
+    json::value BuildPageNotFoundJsonResponse(){
+        json::value root;
+        root.as_object()["code"] = "pageNotFound";
+        root.as_object()["message"] = "Page not found";
+        return root;
+    };
+}
--- sprint1/problems/map_json/solution/src/json_loader.h
+++ sprint1/problems/map_json/solution/src/json_loader.h
@@ -5,7 +5,6 @@
 #include "model.h"
 
 namespace json_loader {
-
 model::Game LoadGame(const std::filesystem::path& json_path);
 
 }  // namespace json_loader
