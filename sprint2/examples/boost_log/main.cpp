598761251779fe717436f4a0502e2b19322ec4b6
Stanislav Polischuk <polischuk.stanislav@gmail.com>
2024-12-03 22:01:35 +0400

    fix logging solution directory2

--- sprint2/examples/boost_log/main.cpp
+++ sprint2/examples/boost_log/main.cpp
@@ -1,10 +1,19 @@
-#include <boost/log/trivial.hpp>
-
+#include <boost/log/trivial.hpp>     // для BOOST_LOG_TRIVIAL
+#include <boost/log/core.hpp>        // для logging::core
+#include <boost/log/expressions.hpp> // для выражения, задающего фильтр
 #include <string_view>
 
 using namespace std::literals;
 
+namespace logging = boost::log;
+void InitBoostLogFilter() {
+    logging::core::get()->set_filter(
+            logging::trivial::severity >= logging::trivial::info
+    );
+}
+
 int main() {
+    InitBoostLogFilter();
     BOOST_LOG_TRIVIAL(trace) << "Сообщение уровня trace"sv;
     BOOST_LOG_TRIVIAL(debug) << "Сообщение уровня debug"sv;
     BOOST_LOG_TRIVIAL(info) << "Сообщение уровня info"sv;
