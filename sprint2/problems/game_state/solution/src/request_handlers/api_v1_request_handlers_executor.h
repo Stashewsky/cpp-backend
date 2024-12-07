68602dd1e1682665378c1112ee034910c406f34b
Stanislav Polischuk <polischuk.stanislav@gmail.com>
2024-12-07 14:48:23 +0400

    fix

--- sprint2/problems/game_state/solution/src/request_handlers/api_v1_request_handlers_executor.h
+++ sprint2/problems/game_state/solution/src/request_handlers/api_v1_request_handlers_executor.h
@@ -4,82 +4,84 @@
 
 #include <functional>
 #include <vector>
+#include <optional>
 
 namespace rh_storage{
 
-namespace net = boost::asio;
+    namespace net = boost::asio;
 
-template<typename Request, typename Send>
-class ApiV1RequestHandlerExecutor{
-    using ActivatorType = bool(*)(const Request&, app::Application&);
-    using HandlerType = void(*)(const Request&, app::Application&, Send&&);
-public:
-    // убираем конструктор копирования
-    ApiV1RequestHandlerExecutor(const ApiV1RequestHandlerExecutor&) = delete;
-    ApiV1RequestHandlerExecutor& operator=(const ApiV1RequestHandlerExecutor&) = delete;
-    ApiV1RequestHandlerExecutor(ApiV1RequestHandlerExecutor&&) = delete;
-    ApiV1RequestHandlerExecutor& operator=(ApiV1RequestHandlerExecutor&&) = delete;
+    template<typename Request, typename Send>
+    class ApiV1RequestHandlerExecutor{
+        using ActivatorType = bool(*)(const Request&);
+        using HandlerType = std::optional<size_t>(*)(const Request&, app::Application&, Send&);
+    public:
+        // убираем конструктор копирования
+        ApiV1RequestHandlerExecutor(const ApiV1RequestHandlerExecutor&) = delete;
+        ApiV1RequestHandlerExecutor& operator=(const ApiV1RequestHandlerExecutor&) = delete;
+        ApiV1RequestHandlerExecutor(ApiV1RequestHandlerExecutor&&) = delete;
+        ApiV1RequestHandlerExecutor& operator=(ApiV1RequestHandlerExecutor&&) = delete;
 
-    // получение ссылки на единственный объект
-    static ApiV1RequestHandlerExecutor& GetInstance() {
-        static ApiV1RequestHandlerExecutor obj;
-        return obj;
-    };
+        // получение ссылки на единственный объект
+        static ApiV1RequestHandlerExecutor& GetInstance() {
+            static ApiV1RequestHandlerExecutor obj;
+            return obj;
+        };
 
-    bool Execute(const Request& req, app::Application& application, Send&& send) {
-        for(auto item : rh_storage_) {
-            if(item.GetActivator()(req, application)){
-                net::dispatch(*application.GetStrand(), [&item, &req, &application, &send]{
-                   item.GetHandler(req.method())(req, application, std::move(send)); 
-                });
-                return true;
+        bool Execute(const Request& req, app::Application& application, Send&& send) {
+            for(auto item : rh_storage_) {
+                if(item.GetActivator()(req)){
+                    net::dispatch(*application.GetStrand(), [&item, &req, &application, &send]{
+                        auto res = item.GetHandler(req.method())(req, application, send);
+                        while(res.has_value()){
+                            res = item.GetEmergeHandlerByIndex(res.value())(req, application, send);
+                        }
+                    });
+                    return true;
+                }
             }
-        }
-        return false;
-    };
+            return false;
+        };
 
-private:
-    
-    std::vector< RequestHandlerNode<ActivatorType, HandlerType> > rh_storage_ = {
-        RequestHandlerNode<ActivatorType, HandlerType>(BadRequestActivator,
-                                                        {{http::verb::get, BadRequestHandler}},
-                                                        BadRequestHandler),
-        RequestHandlerNode<ActivatorType, HandlerType>(GetMapListActivator,
-                                                        {{http::verb::get, GetMapListHandler}},
-                                                        BadRequestHandler),
-        RequestHandlerNode<ActivatorType, HandlerType>(MapNotFoundActivator,
-                                                        {{http::verb::get, MapNotFoundHandler}},
-                                                        BadRequestHandler),
-        RequestHandlerNode<ActivatorType, HandlerType>(GetMapByIdActivator,
-                                                        {{http::verb::get, GetMapByIdHandler}},
-                                                        BadRequestHandler),
-        RequestHandlerNode<ActivatorType, HandlerType>(JoinToGameInvalidJsonReqActivator,
-                                                        {{http::verb::post, JoinToGameInvalidJsonReqHandler}},
-                                                        OnlyPostMethodAllowedHandler),
-        RequestHandlerNode<ActivatorType, HandlerType>(JoinToGameEmptyPlayerNameActivator,
-                                                        {{http::verb::post, JoinToGameEmptyPlayerNameHandler}},
-                                                        OnlyPostMethodAllowedHandler),
-        RequestHandlerNode<ActivatorType, HandlerType>(JoinToGameMapNotFoundActivator,
-                                                        {{http::verb::post, JoinToGameMapNotFoundHandler}},
-                                                        OnlyPostMethodAllowedHandler),
-        RequestHandlerNode<ActivatorType, HandlerType>(JoinToGameActivator,
-                                                        {{http::verb::post, JoinToGameHandler}},
-                                                        OnlyPostMethodAllowedHandler),
-        RequestHandlerNode<ActivatorType, HandlerType>(GetPlayersListEmptyAuthorizationActivator,
-                                                        {{http::verb::get, GetPlayersListEmptyAuthorizationHandler},
-                                                        {http::verb::head, GetPlayersListEmptyAuthorizationHandler}},
-                                                        InvalidMethodHandler),
-        RequestHandlerNode<ActivatorType, HandlerType>(GetPlayersListUnknownTokenActivator,
-                                                        {{http::verb::get, GetPlayersListUnknownTokenHandler},
-                                                        {http::verb::head, GetPlayersListUnknownTokenHandler}},
-                                                        InvalidMethodHandler),
-        RequestHandlerNode<ActivatorType, HandlerType>(GetPlayersListActivator,
-                                                        {{http::verb::get, GetPlayersListHandler},
-                                                        {http::verb::head, GetPlayersListHandler}},
-                                                        InvalidMethodHandler)
-    };
+    private:
 
-    ApiV1RequestHandlerExecutor() = default;
-};
+        std::vector< RequestHandlerNode<ActivatorType, HandlerType> > rh_storage_ = {
+                RequestHandlerNode<ActivatorType, HandlerType>(BadRequestActivator,
+                                                               {{http::verb::get, BadRequestHandler}},
+                                                               BadRequestHandler),
+                RequestHandlerNode<ActivatorType, HandlerType>(GetMapListActivator,
+                                                               {{http::verb::get, GetMapListHandler}},
+                                                               BadRequestHandler),
+                RequestHandlerNode<ActivatorType, HandlerType>(GetMapByIdActivator,
+                                                               {{http::verb::get, GetMapByIdHandler}},
+                                                               BadRequestHandler,
+                                                               {MapNotFoundHandler}),
+                RequestHandlerNode<ActivatorType, HandlerType>(JoinToGameInvalidJsonReqActivator,
+                                                               {{http::verb::post, JoinToGameInvalidJsonReqHandler}},
+                                                               OnlyPostMethodAllowedHandler),
+                RequestHandlerNode<ActivatorType, HandlerType>(JoinToGameEmptyPlayerNameActivator,
+                                                               {{http::verb::post, JoinToGameEmptyPlayerNameHandler}},
+                                                               OnlyPostMethodAllowedHandler),
+                RequestHandlerNode<ActivatorType, HandlerType>(JoinToGameActivator,
+                                                               {{http::verb::post, JoinToGameHandler}},
+                                                               OnlyPostMethodAllowedHandler,
+                                                               {JoinToGameMapNotFoundHandler}),
+                RequestHandlerNode<ActivatorType, HandlerType>(EmptyAuthorizationActivator,
+                                                               {{http::verb::get, EmptyAuthorizationHandler},
+                                                                {http::verb::head, EmptyAuthorizationHandler}},
+                                                               InvalidMethodHandler),
+                RequestHandlerNode<ActivatorType, HandlerType>(GetPlayersListActivator,
+                                                               {{http::verb::get, GetPlayersListHandler},
+                                                                {http::verb::head, GetPlayersListHandler}},
+                                                               InvalidMethodHandler,
+                                                               {UnknownTokenHandler}),
+                RequestHandlerNode<ActivatorType, HandlerType>(GetGameStateActivator,
+                                                               {{http::verb::get, GetGameStateHandler},
+                                                                {http::verb::head, GetGameStateHandler}},
+                                                               InvalidMethodHandler,
+                                                               {UnknownTokenHandler})
+        };
+
+        ApiV1RequestHandlerExecutor() = default;
+    };
 
 }
