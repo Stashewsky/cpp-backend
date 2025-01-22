6fadc4bd6ec5a583b4a1837aedca1921598fb5fe
Stanislav Polischuk <polischuk.stanislav@gmail.com>
2025-01-22 14:20:53 +0400

    raw diploma

---
+++ sprint4/problems/leave_game/solution/src/records_player.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "tagged_uuid.h"
+
+namespace app{
+    namespace detail {
+        struct RecordsPlayerTag {};
+    }  // namespace detail
+
+    using RecordsPlayerId = util::TaggedUUID<detail::RecordsPlayerTag>;
+
+    struct RecordsPlayer{
+        RecordsPlayerId id;
+        std::string name;
+        int score = 0;
+        int play_time_ms = 0;
+    };
+
+    class RecordsPlayerRepository {
+    public:
+        virtual void Save(const RecordsPlayer& record) = 0;
+        virtual std::vector<RecordsPlayer> Read() = 0;
+        virtual std::vector<RecordsPlayer> Read(int start, int maxItems ) = 0;
+    };
+}
